c1afce19e8bf3cea390fd285bd32b4f7d8b58a67
Cocomows <corentin.caradec@gmail.com>
2019-03-02 14:46:21 +0100

    changed default route

--- apresderniere/urls.py
+++ apresderniere/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
+    path('', include('movies.urls')),
 
 ]
